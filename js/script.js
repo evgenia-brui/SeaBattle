3a40cad0174697bad47934f7c6518d18988129e9
Evgenia Brui <j-lp@mail.ru>
2020-03-06 23:37:56 +0300

    Day 3

--- js/script.js
+++ js/script.js
@@ -7,25 +7,78 @@ const again = document.getElementById('again');
 const header = document.querySelector('.header');
 
 const game = {
-    ships: [
-        {
-            location: ['26', '36', '46', '56'],
-            hit: ['', '', '', '']
-        },
-        {
-            location: ['11', '12', '13'],
-            hit: ['', '', '']
-        },
-        {
-            location: ['69', '79'],
-            hit: ['', '']
-        },
-        {
-            location: ['32'],
-            hit: ['']
+    ships: [],
+    shipCount: 0,
+    optionShip: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1]
+    },
+    collision: new Set(),
+    generateShip() {
+        for (let i = 0; i < this.optionShip.count.length; i++) {
+            for (let j = 0; j < this.optionShip.count[i]; j++) {
+                const size = this.optionShip.size[i];
+                const ship = this.generateOptionShip(size);
+                this.ships.push(ship);
+                this.shipCount++;
+            }
         }
-    ],
-    shipCount: 4
+    },
+    generateOptionShip(shipSize) {
+        const ship = {
+            hit: [],
+            location: [],
+        };
+
+        const direction = Math.random() < 0.5;
+        if (direction) {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize));
+        } else {
+            x = Math.floor(Math.random() * (10 - shipSize));
+            y = Math.floor(Math.random() * 10);
+        }
+
+        for (let i = 0; i < shipSize; i++) {
+            if (direction) {
+                ship.location.push(x + '' + (y + i));
+            } else {
+                ship.location.push((x + i) + '' + y);
+            }
+            ship.hit.push('');
+        }
+
+        if (this.checkCollision(ship.location)) {
+            return this.generateOptionShip(shipSize);
+        }
+
+        this.addCollision(ship.location);
+
+        return ship;
+    },
+    checkCollision(location) {
+        for (const coord of location) {
+            if (this.collision.has(coord)) {
+                return true;
+            }
+        }
+    },
+    addCollision(location) {
+        for (let i = 0; i < location.length; i++) {
+            const startCoordX = location[i][0] - 1;
+
+            for (let j = startCoordX; j < startCoordX + 3; j++) {
+                const startCoordY = location[i][1] - 1;
+
+                for (let z = startCoordY; z < startCoordY + 3; z++) {
+                    if (j >= 0 && j < 10 && z >= 0 && z < 10) {
+                        const coord = j + '' + z;
+                        this.collision.add(coord);
+                    }
+                }
+            }
+        }
+    }
 };
 
 const play = {
@@ -98,11 +151,17 @@ const fire = (event) => {
 };
 
 const init = () => {
-    enemy.addEventListener('click', fire);
     play.render();
+    game.generateShip();
 
+    enemy.addEventListener('click', fire);
     again.addEventListener('click', () => {
         location.reload();
+    });
+    record.addEventListener('dblclick', () => {
+        localStorage.clear();
+        play.record = 0;
+        play.render();
     });
 };
 
